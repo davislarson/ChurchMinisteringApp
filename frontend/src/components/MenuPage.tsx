d865008b802ee22f25ef7047d7e319da12cf1698
Davis Larson <davislarson01@gmail.com>
2025-03-12 14:39:32 -0600

    add menu page with nav buttons

---
+++ frontend/src/components/MenuPage.tsx
@@ -0,0 +1,61 @@
+import NavigationItem from "./NavigationItem";
+
+function MenuPage() {
+	const navigationItems = [
+		{
+			icon: (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					fill="currentColor"
+					className="bi bi-house"
+					viewBox="0 0 16 16"
+				>
+					<path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z" />
+				</svg>
+			),
+			label: "Home",
+		},
+		{
+			icon: (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					fill="currentColor"
+					className="bi bi-calendar"
+					viewBox="0 0 16 16"
+				>
+					<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
+				</svg>
+			),
+			label: "Calendar",
+		},
+		{
+			icon: (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					fill="currentColor"
+					className="bi bi-lightbulb-fill"
+					viewBox="0 0 16 16"
+				>
+					<path d="M2 6a6 6 0 1 1 10.174 4.31c-.203.196-.359.4-.453.619l-.762 1.769A.5.5 0 0 1 10.5 13h-5a.5.5 0 0 1-.46-.302l-.761-1.77a2 2 0 0 0-.453-.618A5.98 5.98 0 0 1 2 6m3 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1l-.224.447a1 1 0 0 1-.894.553H6.618a1 1 0 0 1-.894-.553L5.5 15a.5.5 0 0 1-.5-.5" />
+				</svg>
+			),
+			label: "Ideas/ AI",
+		}
+	];
+
+	return (
+		<section className="p-4 mt-4 w-100 bg-black rounded shadow-sm">
+			{navigationItems.map((item, index) => (
+				<NavigationItem key={index} icon={item.icon} label={item.label} />
+			))}
+		</section>
+	);
+}
+
+export default MenuPage;
