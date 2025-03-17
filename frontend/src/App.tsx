ae998e86f3c6f3cec4b61c5ca4ad85917836b33a
ashleebingham <ashleeb9@byu.edu>
2025-03-17 16:40:20 -0600

    Calendar page creation and css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import CalendarPage from "./pages/CalendarPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <CalendarPage />
     </>
   );
 }
