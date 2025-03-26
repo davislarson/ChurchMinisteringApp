0a703d97de1c669ae9d5494911100724def32c9e
drew <drewc@byu.edu>
2025-03-25 20:41:23 -0600

    Updates to database and program.cs/controllers

--- backend/ChurchMinisteringApp/ChurchMinisteringApp/Program.cs
+++ backend/ChurchMinisteringApp/ChurchMinisteringApp/Program.cs
@@ -1,25 +1,21 @@
+﻿using ChurchMinisteringApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<MinisteringContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("MinisteringConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
+app.MapControllers(); // 💥 This MUST be here
 
-app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
